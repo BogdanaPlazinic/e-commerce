7bd0222ac9aafa471c2b10e61ef7f908b15aeb2a
Bogdana <plazinicbogdana@gmail.com>
2024-01-02 22:21:00 +0100

    Footer component

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 import { Footer } from '../../../payload/payload-types'
-import { fetchFooter, } from '../../_api/fetchGlobals'
-import { FooterComponent } from '../Footer/FooterComponent'
-
+import { fetchFooter } from '../../_api/fetchGlobals'
+import FooterComponent from './FooterComponent'
 
 export async function Footer() {
   let footer: Footer | null = null
@@ -19,7 +18,7 @@ export async function Footer() {
 
   return (
     <>
-    <FooterComponent footer={footer} />
+      <FooterComponent footer={footer} />
     </>
   )
 }
